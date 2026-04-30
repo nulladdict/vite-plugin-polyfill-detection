e6c8411dce2164dec9ff9b8841d2306a9fed4791
nulladdict <nulladdicted@gmail.com>
2026-04-30 10:08:46 +0200

    fix: update dependencies

--- test/fixture/main.js
+++ test/fixture/main.js
@@ -1,4 +1,7 @@
 const flat = [1, [2, 3]].flat();
-const entries = Object.fromEntries([["a", 1], ["b", 2]]);
+const entries = Object.fromEntries([
+  ["a", 1],
+  ["b", 2],
+]);
 const text = "hello world".replaceAll("o", "0");
 console.log(flat, entries, text);
